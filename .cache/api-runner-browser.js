36c04673459fb5d1f35a48132e2ee5fd3a4d4383
Lev <levdevuk@gmail.com>
2018-03-18 21:31:54 +0000

    adds NetlifyCMS

--- .cache/api-runner-browser.js
+++ .cache/api-runner-browser.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('/Users/lev/dev/gatsbypractice-lut/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks
 // basically like:
 // var plugins = [
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    desc: 'A new blog',
+    title: "Gatsby Practice Site",
+    desc: "Finding my feet with React, Gatsby and Styled Components"
   },
-  pathPrefix: '/gatsby-practice-1',
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'src',
-        path: `${__dirname}/src/`,
-      },
+        name: "src",
+        path: `${__dirname}/src/`
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'img',
-        path: `${__dirname}/src/images`,
-      },
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
-        excerpt_separator: '<!-- end -->',
-      },
+        excerpt_separator: "<!-- end -->"
+      }
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-  ],
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-netlify-cms"
+  ]
 };
+
+// pathPrefix: "/gatsby-practice-1",
