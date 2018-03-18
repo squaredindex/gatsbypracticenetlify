66ed430ad2c471a2a98f750d2d9bbe0541570d83
Lev <levdevuk@gmail.com>
2018-03-18 21:49:45 +0000

    build update

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,7 +8,6 @@ exports.layouts = {
 
 exports.components = {
   "component---src-posts-post-page-js": preferDefault(require("/Users/lev/dev/gatsbypractice-lut/src/posts/PostPage.js")),
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/lev/dev/gatsbypractice-lut/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/lev/dev/gatsbypractice-lut/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/lev/dev/gatsbypractice-lut/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/lev/dev/gatsbypractice-lut/src/pages/index.js"))
@@ -19,8 +18,6 @@ exports.json = {
   "posts-post.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/posts-post.json"),
   "layout-index.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/layout-index.json"),
   "posts-our-second-post.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/posts-our-second-post.json"),
-  "layout-index.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/dev-404-page.json"),
   "layout-index.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/layout-index.json"),
   "404.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/404.json"),
   "layout-index.json": require("/Users/lev/dev/gatsbypractice-lut/.cache/json/layout-index.json"),
